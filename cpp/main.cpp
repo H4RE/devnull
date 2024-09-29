c400712fc08db7ae4e4fc2b838a2d4ea4eca239c
H4RE <h4re.hpp@gmail.com>
2024-09-29 20:02:18 +0900

    update

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,21 +1,111 @@
 #include <iostream>
 
-void func();
+class Base1
+{
+public:
+    void wo_vi_ov(){std::cout << "wo vi ov @base" <<std::endl;}
+};
+
+class Sub1: public Base1
+{
+public:
+    void wo_vi_ov(){std::cout << "wo vi ov @sub" <<std::endl;}
+};
+
+class Base2
+{
+public:
+    void wo_vi_ov(){std::cout << "wo vi ov @base" <<std::endl;}
+
+};
+
+class Sub2: public Base2
+{
+public:
+    // これはコンパイルエラー
+    // void wo_vi_ov()override{std::cout << "wo vi ov @sub" <<std::endl;}
+};
+
+class Base3
+{
+public:
+    virtual void wo_vi_ov(){std::cout << "wo vi ov @base" <<std::endl;}
+
+};
+
+class Sub3: public Base3
+{
+public:
+    void wo_vi_ov(){std::cout << "wo vi ov @sub" <<std::endl;}
+};
+
+class Base4
+{
+public:
+    virtual void wo_vi_ov(){std::cout << "wo vi ov @base" <<std::endl;}
+
+};
+
+class Sub4: public Base4
+{
+public:
+    void wo_vi_ov()override{std::cout << "wo vi ov @sub" <<std::endl;}
+};
 int main(int argc, char** argv)
 {
-    func();
+    // 1: virtualなし overrideなし
+    {
+        Sub1 sub;
+        sub.wo_vi_ov();//subが呼ばれる
+
+        Base1* base = &sub;
+        // 再定義なので、baseのポインタ経由ではbaseが呼ばれる
+        base->wo_vi_ov();
+    }
+
+    // 2: virtualなし overrideあり
+    {
+    }
+
+    // 3: virtualあり overrideなし
+    {
+        Sub3 sub;
+        sub.wo_vi_ov();//subが呼ばれる
+
+        Base3* base = &sub;
+        // baseにvirtualがついているのでsubが呼ばれる
+        base->wo_vi_ov();//subが呼ばれる
+    }
+    // 4: virtualあり overrideなし
+    {
+        Sub4 sub;
+        sub.wo_vi_ov();//subが呼ばれる
+
+        Base4* base = &sub;
+        // baseにvirtualがついているのでsubが呼ばれる
+        base->wo_vi_ov();//subが呼ばれる
+    }
     return 0;
 }
+
 /*
-- 宣言だけだと、コンパイルはできてその後undefined symbolsでリンクエラー
-
-- a.cppにfuncの定義がある場合、コンパイルできてa.cppのfuncが実行される。
-
-- a.cppとb.cppにfuncの定義があると、duplicate symbolsでリンクエラー
-
-    - b.cppの関数がvoid func(int a);であればコンパイルできaのfuncが実行される
-        int func(void)だとダメ。
-
-- a側にstaticをつけると、コンパイルできbのfuncが呼び出される
-
- */
+ *
+ * Base Class
+ * virtual あり　なし
+ * Sub　Class
+ * override　あり　なし
+ *
+ * 1. virtual なし and override なし
+ * 関数の再定義になる
+ * Subでインスタンスして呼び出すとSubの関数が呼ばれるが、
+ * Baseのポインタ経由で呼び出すとbaseの関数が呼ばれる
+ *
+ * 2. virtual なし and override あり
+ * overrideするものがないのでコンパイルエラー
+ *
+ * 3. virtual あり and override なし
+ * 4. virtual あり and override あり
+ * どちらもSubが呼ばれる
+ * baseのポインタ経由でもvirtualが付いているのでsubが呼ばれる
+ *
+ * */
